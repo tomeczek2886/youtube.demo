9f145febeb872529b49bf7e480608623a863162f
Tomasz Nowak <tomeczek2886@gmail.com>
2021-08-03 11:14:14 +0200

    More changes to index

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, this is a test of github");
+        System.out.printf("%d apples", 3);
     }
 }
